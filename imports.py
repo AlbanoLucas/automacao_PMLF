1b10bc70a7d356b9a9020f1b2b9ba81ba51192c4
AlbanoLucas <albanolucas23@gmail.com>
2025-02-18 23:25:21 -0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -8,5 +8,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import pandas as pd
 import time
 import pyautogui
