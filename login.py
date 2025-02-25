d7d0467ead6fdb59f41350c43269638b5cd80801
AlbanoLucas <albanolucas23@gmail.com>
2025-02-25 16:54:21 -0300

    Update login.py

--- login.py
+++ login.py
@@ -3,6 +3,26 @@ from webdriver_setup import webdriver_setup
 from read_xls import carregar_dados
 
 
+def equiparar():
+    driver, wait = webdriver_setup()
+    ###################################  EQUIPARAR  ###################################
+    #Botao Pesquisar
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="mostrarBotaoBusca"]/input'))).click()
+
+    #Pesquisa Nome
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layerBusca"]/table/tbody/tr[3]/td[2]/input'))).send_keys("Albano Lucas Evangelista de Souza")
+    wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="layerBusca"]/table/tbody/tr[7]/td[1]/input[1]'))).click()
+
+    #Seleciona Usuario
+    wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[3]/form/table[3]/tbody/tr[2]/td[2]/a'))).click()
+    perfis = driver.find_element(By.ID, 'perfisId')
+    atribuicoes = perfis.find_elements(By.TAG_NAME, 'option')
+    lista_atribuicoes = []
+    for atribuicao in atribuicoes:
+        lista_atribuicoes.append(atribuicao.text)
+    driver.find_element(By.XPATH, '/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[3]/form/table/tbody/tr[3]/td/input[2]')
+    return lista_atribuicoes
+
 def cadastro(dados_completos):
     for dado in dados_completos:
 
@@ -37,6 +57,9 @@ def cadastro(dados_completos):
         wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="abaSeguranca"]/td[2]'))).click()
         wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="abaSegurancaControle"]/td[2]'))).click()
         wait.until(EC.visibility_of_element_located((By.XPATH, '//*[@id="divSGnivelControle"]/table/tbody/tr[2]/td/a'))).click()
+
+        if  != None:
+            atribuicoes = equiparar()
         #Botao Incluir
         includ_button = wait.until(EC.visibility_of_element_located((By.XPATH, '/html/body/table/tbody/tr[2]/td/table/tbody/tr/td[3]/form/table[2]/tbody/tr/td[1]/input[2]')))
         includ_button.click()
