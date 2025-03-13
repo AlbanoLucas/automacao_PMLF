14d9c928d8ad99bb095298eff12bcf4800ab484b
AlbanoLucas <albanolucas23@gmail.com>
2025-03-13 07:39:15 -0300

    Create www.py

---
+++ www.py
@@ -0,0 +1,21 @@
+from imports import *
+
+
+edicoes = []
+data_atual = datetime.now().strftime('%d/%m/%Y')
+url = "https://laurodefreitas.ba.gov.br/2022/"
+chrome_options = Options()
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
+driver.get(url)
+time.sleep(2)
+driver.find_element(By.XPATH, '/html/body/header/div/div/div[1]/div/div/div/div[2]/a[2]/button').click()
+time.sleep(10)
+app_ed_ant = driver.find_element(By.XPATH, '/html/body/app-root/app-homepage/app-edicoes-anteriores')
+tabela_elements = app_ed_ant.find_elements(By.TAG_NAME, 'tr')
+for linha in tabela_elements:
+    linha_elements = linha.find_elements(By.TAG_NAME, 'td')
+    if linha_elements[0].text == data_atual:
+        edicoes.append(linha_elements[1].text)
+    else:
+        break
+print(edicoes)
