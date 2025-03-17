783b400124358d17ba37c821c0a0d5852d16f6da
AlbanoLucas <albanolucas23@gmail.com>
2025-03-17 17:00:14 -0300

    Update imports.py

--- imports.py
+++ imports.py
@@ -3,25 +3,17 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-import pandas as pd
 import time
 import pyautogui
 import PyPDF2
 import re
 import selenium
 import requests 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from datetime import datetime  
-import os
-import re
-import PyPDF2
-from datetime import datetime
 from playwright.sync_api import sync_playwright
+import os
+import csv
+import pandas as pd
