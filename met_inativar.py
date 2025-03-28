ba25c3f16d9d1791d33b204c1ab42fe037a91845
AlbanoLucas <albanolucas23@gmail.com>
2025-03-28 16:07:22 -0300

    Delete met_inativar.py

--- met_inativar.py
+++
@@ -1,8 +0,0 @@
-from imports import *
-from webdriver_setup import webdriver_setup
-
-
-driver, wait = webdriver_setup()
-url = "https://lfrh.metropolisweb.com.br/metropolisWEB/"
-driver.get(url)
-time.sleep(5)
