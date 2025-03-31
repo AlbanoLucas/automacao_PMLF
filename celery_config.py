e86a0cad0d54e70e03049a42efb8fbaa61197348
AlbanoLucas <albanolucas23@gmail.com>
2025-03-30 22:38:09 -0300

    Update celery_config.py

--- celery_config.py
+++ celery_config.py
@@ -1,13 +1,22 @@
 from celery import Celery
+from celery.schedules import crontab
+
 
 # Configure o Celery com o broker Redis
 app = Celery('diario', broker='redis://localhost:6379/0')
 
 # Configuração adicional
 app.conf.update(
-    result_backend='redis://localhost:6379/0',
-    task_serializer='json',
+    result_backend='redis://localhost:6379/0',  # Redis como backend para resultados
+    task_serializer='json',  # Formato de serialização das tarefas
+    timezone='America/Sao_Paulo',  # Definindo o fuso horário para o Brasil
+    enable_utc=True,  # Utilizar UTC para todas as tarefas
 )
-app.conf.update(
-    worker_pool='prefork',  # Muda o pool para 'prefork', que pode ser mais adequado para sistemas Windows
-)
+
+# Adicionalmente, se for necessário configurar o Celery Beat, faça assim:
+app.conf.beat_schedule = {
+    'executar-tarefa': {
+        'task': 'tasks.run_my_script',  # Nome da tarefa a ser agendada
+        'schedule': crontab(hour=8, minute=30, day_of_week='1-5'),  # Segunda a sexta às 8:30
+    },
+}
